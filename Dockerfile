940b7be72c95e24c496181ccb692f52f7c9662ca
bnhf <41088895+bnhf@users.noreply.github.com>
2023-02-03 09:35:26 -0700

    Added logging for config file states

--- Dockerfile
+++ Dockerfile
@@ -11,10 +11,10 @@ RUN echo Starting. \
  && apt-get -q -y install --no-install-recommends apcupsd dbus libapparmor1 libdbus-1-3 libexpat1 tzdata \
  && apt-get -q -y full-upgrade \
  && rm -rif /var/lib/apt/lists/* \
- && mv /usr/local/bin/apcupsd         /etc/default/apcupsd \
- && mv /usr/local/bin/apcupsd.conf    /etc/apcupsd/apcupsd.conf \
- && mv /usr/local/bin/hosts.conf      /etc/apcupsd/hosts.conf \
- && mv /usr/local/bin/doshutdown      /etc/apcupsd/doshutdown \
+# && mv /usr/local/bin/apcupsd         /etc/default/apcupsd \
+# && mv /usr/local/bin/apcupsd.conf    /etc/apcupsd/apcupsd.conf \
+# && mv /usr/local/bin/hosts.conf      /etc/apcupsd/hosts.conf \
+# && mv /usr/local/bin/doshutdown      /etc/apcupsd/doshutdown \
  && echo Finished.
 
-CMD ["/sbin/apcupsd", "-b"]
+CMD /start.sh
--- start.sh
+++ start.sh
@@ -6,25 +6,32 @@
 # Check if apcupsd.conf already exists
 if [ ! -f /etc/apcupsd/apcupsd.conf ]; then
   mv /usr/local/bin/apcupsd /etc/default/apcupsd \
-  && mv /usr/local/bin/apcupsd.conf /etc/apcupsd/apcupsd.conf 
-else
+  && mv /usr/local/bin/apcupsd.conf /etc/apcupsd/apcupsd.conf \
+  && echo "No existing apcupsd.conf found"
+  else
   mv /usr/local/bin/apcupsd /etc/default/apcupsd \
-  && rm /usr/local/bin/apcupsd.conf
+  && rm /usr/local/bin/apcupsd.conf \
+  && echo "Existing apcupsd.conf found, and will be used"
 fi
 
 # Check if hosts.conf already exists
 if [ ! -f /etc/apcupsd/hosts.conf ]; then
-  mv /usr/local/bin/hosts.conf /etc/default/hosts.conf
+  mv /usr/local/bin/hosts.conf /etc/default/hosts.conf \
+  && echo "No existing hosts.conf found"
 else
-  rm /usr/local/bin/hosts.conf
+  rm /usr/local/bin/hosts.conf \
+  && echo "Existing hosts.conf found, and will be used"
 fi
 
 # Check if doshutdown already exists
 if [ ! -f /etc/apcupsd/doshutdown ]; then
-  mv /usr/local/bin/doshutdown /etc/default/doshutdown
+  mv /usr/local/bin/doshutdown /etc/default/doshutdown \
+  && echo "No existing doshutdown found"
 else
-  rm /usr/local/bin/doshutdown
+  rm /usr/local/bin/doshutdown \
+  && echo "Existing doshutdown found, and will be used"
 fi
+
 # Check if UPSNAME environment variable is set, and if so update apcupsd.conf
 if [ ! -z $UPSNAME ]; then
   sed -i 's|^UPSNAME.*|UPSNAME '"$UPSNAME"'|' /etc/apcupsd/apcupsd.conf
